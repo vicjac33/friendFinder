0ba62d9bfb2d3e0e353f84c4017339562cef0ec2
Victoria Puente <puentevic33@gmail.com>
2019-03-09 00:02:09 -0600

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Jose",
+    "photo" : "https://amp.businessinsider.com/images/5967a021552be551008b4771-750-563.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Gracie",
+    "photo" : "https://www.sheknows.com/wp-content/uploads/2018/12/alpha-woman-600-wide_lblpwl.jpeg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Tyler",
+    "photo" : "https://theweal.com/wp-content/uploads/2014/10/750SEC-headshots-Orientation-Issue-173c-800.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Allysa",
+    "photo" : "http://www.tenerewilliams.com/wp-content/uploads/2013/02/Naomi-Grossman-headshot.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "David",
+    "photo" : "https://learn.g2crowd.com/hubfs/Team%20Photos/Research/pszakiel.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kandy",
+    "photo" : "https://cdn1.medicalnewstoday.com/content/images/articles/320/320137/smiling-middle-aged-woman.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Polly",
+    "photo" : "http://igmodelnews.com/wp-content/uploads/2017/10/1-2.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Andie",
+    "photo" : "https://img.freepik.com/free-photo/portrait-happy-woman-smiling_1303-9953.jpg?size=626&ext=jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Josie",
+    "photo" : "http://www.viraltrendzz.com/wp-content/uploads/2018/02/Doutzen-Kroes-Most-Beautiful-Dutch-Woman.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Liza",
+    "photo" : "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Tino",
+    "photo" : "https://www.sss.gov/portals/0/Born%20After%201960.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+    "photo" : "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Richard",
+    "photo" : "https://www.telegraph.co.uk/content/dam/news/2018/05/12/TELEMMGLPICT000161569898_trans_NvBQzQNjv4Bqe5jTtuvhyhg4PHi86V8a0p815QE7DqplUJlHpAWoYtg.jpeg?imwidth=1400",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Amelia",
+    "photo" : "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
